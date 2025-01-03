6b0d57a1e68ceeb0e4242d6ef5d97774ab210779
Lenovo <Lenovo@LAPTOP-GCC8HOTA>
2025-01-03 17:46:00 +0300

    Add project files.

---
+++ YummyProject/Context/YummyContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using YummyProject.Models;
+
+namespace YummyProject.Context
+{
+    public class YummyContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<ChefSocial> ChefSocials { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<PhotoGallery> photoGalleries { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> testimonials { get; set; }
+
+
+    }
+}
