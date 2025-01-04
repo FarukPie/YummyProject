4d265638b3c897ebde8891939bf7be27bb9b179f
Lenovo <Lenovo@LAPTOP-GCC8HOTA>
2025-01-04 20:46:14 +0300

    update

--- YummyProject/Controllers/DefaultController.cs
+++ YummyProject/Controllers/DefaultController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YummyProject.Context;
 
 namespace YummyProject.Controllers
 {
     public class DefaultController : Controller
     {
-       
+       YummyContext context=new YummyContext();
         public ActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult DefaultFeature()
+        {   var value=context.Features.ToList();
+            return PartialView(value);
+        }
+
+        public PartialViewResult DefaultAbout()
+        {
+            var value=context.Abouts.ToList();
+            return PartialView(value);
+        }
+        public PartialViewResult DefaultService()
+        {
+            var value=context.Services.ToList();
+            return PartialView(value);
+        }
+
+        public PartialViewResult DefaultProduct()
+        {
+            var value=context.products.ToList();
+            return PartialView(value);  
+        }
+        public PartialViewResult DefaultTestimonial()
+        {
+            var value=context.testimonials.ToList();
+            return PartialView(value);
+
         }
     }
 }
