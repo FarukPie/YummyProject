e9474c3ba70e94033f873db62f14c67736b6b734
FarukPie <farukatak.official@gmail.com>
2025-07-21 18:52:50 +0300

    I created validation migration and I have completed feature back section

---
+++ YummyProject/Controllers/FeatureController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using YummyProject.Context;
+using YummyProject.Models;
+
+namespace YummyProject.Controllers
+{
+    public class FeatureController : Controller
+    {
+        YummyContext context=new YummyContext();
+        public ActionResult Index()
+        {
+            var value=context.Features.ToList();
+            return View(value);
+        }
+        public ActionResult AddFeature()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddFeature(Feature feature)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(feature);
+            }
+
+            context.Features.Add(feature);
+            int result=context.SaveChanges();
+            if(result==0)
+            {
+                ModelState.AddModelError("","error");
+                return View(feature);
+            }
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult DeleteFeature(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); // 400 Bad Request
+            }
+
+            var value = context.Features.Find(id);
+            if (value == null)
+            {
+                return HttpNotFound(); // 404 Not Found
+            }
+
+            context.Features.Remove(value);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- YummyProject/Models/Feature.cs
+++ YummyProject/Models/Feature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace YummyProject.Models
     public class Feature
     {
         public int FeatureID { get; set; }
+
+        [Required(ErrorMessage ="image Url is not be empty")]
         public string ImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Title is not be empty")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is not be empty")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Video Url is not be empty")]
         public string VideoUrl { get; set; }
     }
 }
