a3a66f3484dc1f8e407b3bca91a98417eb7de160
FarukPie <farukatak.official@gmail.com>
2025-07-31 01:30:11 +0300

    added event controller

---
+++ YummyProject/Controllers/EventController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YummyProject.Context;
+
+namespace YummyProject.Controllers
+{
+    public class EventController : Controller
+    {
+            YummyContext context=new YummyContext();
+        public ActionResult Index()
+        {
+            var value=context.Events.ToList();
+            return View(value);
+        }
+    }
+}
